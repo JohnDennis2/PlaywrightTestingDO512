f0480d0a093a8fd62573d9a88a9653e13cc1e20f
John D <johnd@Johns-MBP.attlocal.net>
2024-07-26 14:20:40 -0500

    serach test

--- tests/do512.spec.ts
+++ tests/do512.spec.ts
@@ -4,3 +4,6 @@ import { test, expect } from '@playwright/test'
 test('navigate to DO512', async ({page}) => {
     await page.goto('https://do512.com/')
 })
+test('Get search bar', async ({page}) =>{
+    await page.getByRole('link',({name:'search'}))
+})
