2a0815a044685823068c4a36ad5a40ba79b8206b
John D <johnd@Johns-MBP.attlocal.net>
2024-07-26 14:35:49 -0500

    food search

--- tests/do512.spec.ts
+++ tests/do512.spec.ts
@@ -2,8 +2,13 @@ import { test, expect } from '@playwright/test'
 
 
 test('navigate to DO512', async ({page}) => {
-    await page.goto('https://do512.com/')
+    await page.goto('https://do512.com/');
 })
 test('Get search bar', async ({page}) =>{
-    await page.getByRole('link',({name:'search'}))
+    await page.getByRole('link',({name:'search'}));
+
+    await page.fill('input,[name="search"]','Food');
+
+    const searchValue = await page.inputValue('input[name="search"]');
+    expect(searchValue).toBe('Food');
 })
